9fccbee1584ca12cd4bc2cef099dde7c27ff91b2
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-22 20:56:30 +0100

    Entrega02

--- Entrega02/Program.cs
+++ Entrega02/Program.cs
@@ -6,7 +6,18 @@ namespace Entrega02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int num1 = 5;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es: " + valor1);           
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
         }
     }
 }
